1b23f664deeab4f81aed9a3d563b3673a03af9d6
Kush Sharma <kushsharma1001@gmail.com>
2021-03-02 19:14:29 +0530

    Add files via upload
    2nd commit

---
+++ SumTest.java
@@ -0,0 +1,60 @@
+package com.operation;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+
+public class SumTest {
+
+	private Sum sum = new Sum();
+
+	private int result = 0;
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void calculate_Positive_Null() {
+
+		// input: null
+
+		result = sum.calculate(null);
+		Assert.assertEquals(0, result);
+	}
+
+	@Test
+	public void calculate_Positive_SingleNumber() {
+		// input:2
+		result = sum.calculate("2");
+		Assert.assertEquals(2, result);
+	}
+	
+	@Test
+	public void calculate_Positive() {
+		// input:2
+		result = sum.calculate("2;8");
+		Assert.assertEquals(10, result);
+	}
+
+	@Test
+	public void calculate_Negative_InvalidColon() {
+
+		// input: :
+		expectedException.expect(InvalidInputException.class);
+		expectedException.expectMessage("Invalid input: " + ":");
+		result = sum.calculate(":");
+	}
+	
+	@Test
+	public void calculate_Negative_InvalidAlphabets() {
+
+		// input: :
+		expectedException.expect(InvalidInputException.class);
+		expectedException.expectMessage("Invalid input: " + "2;b");
+		result = sum.calculate("2;b");
+	}
+}
